89da0e1abffbf32aa21afc5bd8dfd163a398caf0
FranciscoJunior12 <105241871+FranciscoJunior12@users.noreply.github.com>
2024-05-30 13:21:40 +0200

    feat: update details pages

--- web/src/pages/EventsDetails/EventsDetails.jsx
+++ web/src/pages/EventsDetails/EventsDetails.jsx
@@ -3,6 +3,9 @@ import { useSearchParams, Link } from 'react-router-dom';
 import axios from 'axios';
 import Back from '../../assets/Back.png';
 import DataWave from '../../assets/DataWave.png';
+import speaker from '../../assets/Speaker.png';
+import speaker1 from '../../assets/speaker1.png';
+import speaker2 from '../../assets/Speaker2.png';
 import './EventsDetails.css';
 import { EventSignUp } from '../EventSignUp/EventSignUp';
 
@@ -12,6 +15,13 @@ export const EventsDetails = () => {
     const query = useParams.get("q");
     const url = import.meta.env.VITE_SOME_KEY;
     const api = axios.create({ baseURL: url, withCredentials: true });
+
+    const handleClick = () => {
+
+
+
+        
+    }
 
     useEffect(() => {
         if (query) {
@@ -53,7 +63,87 @@ export const EventsDetails = () => {
                     <p className="Descricao">
                         {event.description}
                     </p>
+                    <div className='goals'>
+                        <h3>OBJECTIVOS DO EVENTO</h3>
+                        <div id="goals">
+                            <div id="h1">
+                                <h1>1</h1>
+                            </div>
+                            <p>
+                                Explorar oportunidades e desafios no ramo de ciência de dados no contexto local
+                            </p>
+                        </div>
+                        <div id="goals">
+                            <div id="h1">
+                                <h1>2</h1>
+                            </div>
+                            <p>
+                                Explorar oportunidades e desafios no ramo de ciência de dados no contexto local
+                            </p>
+                        </div>
+                        <div id="goals">
+                            <div id="h1">
+                                <h1>3</h1>
+                            </div>
+                            <p>
+                                Explorar oportunidades e desafios no ramo de ciência de dados no contexto local
+                            </p>
+                        </div>
+                    </div>
+
+
+
+                    <div className="ProximosEventos">
+                        <h2>ORADDORES</h2>
+                        <section className="BoxEventos">
+
+
+                            <Link className="BoxEventoConteudo" to={`/events?q=${event.id}`}>
+                                <div className="overlay">
+
+                                    <img className="ImagemEvento" src={speaker} alt="Icone Evento" />
+                                    <article>
+                                        <h2 className="NomeEvento">ABNEUSA MANUEL</h2>
+
+                                        <p className="LocalEvento">M-Pesa Senior Business Analyst</p>
+                                    </article>
+                                </div>
+                            </Link>
+                            <Link className="BoxEventoConteudo" to={`/events?q=${event.id}`}>
+                                <div className="overlay">
+
+                                    <img className="ImagemEvento" src={speaker2} alt="Icone Evento" />
+                                    <article>
+                                        <h2 className="NomeEvento">MARGARIDA</h2>
+
+                                        <p className="LocalEvento">BRR Enginee  - Vodacom Moçambique</p>
+                                    </article>
+                                </div>
+                            </Link>
+                            <Link className="BoxEventoConteudo" to={`/events?q=${event.id}`}>
+                                <div className="overlay">
+
+                                    <img className="ImagemEvento" src={speaker1} alt="Icone Evento" />
+                                    <article>
+                                        <h2 className="NomeEvento">LAUDIO MUFUME</h2>
+
+                                        <p className="LocalEvento">Gestor da Área de Dados - Vodacom</p>
+                                    </article>
+                                </div>
+                            </Link>
+
+
+
+
+                        </section>
+                    </div>
+
+
+                    <input type="submit" value="Inscreve-se" className="Submeter" onClick={handleClick} />
                 </section>
+
+
+
             </div>
         </>
     );
--- web/src/pages/Home/Home.jsx
+++ web/src/pages/Home/Home.jsx
@@ -29,7 +29,7 @@ export const Home = () => {
     }
 
     function handleLogout() {
-        logout();
+        logout(); 
     }
 
 
